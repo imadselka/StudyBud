0a810760f44a62e6f64fa273ce38ef5d8259e020
Imad Selka <imadselkawi133@gmail.com>
2024-02-10 21:50:53 +0100

    add dynamic rendering

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from base.constants.constants import rooms
+
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, "base/home.html", {"rooms": rooms})
 
-def room(request):
-    return render(request,'room.html')
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}
+    return render(request, "base/room.html", context)
