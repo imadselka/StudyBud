c7ab473dee43dd8daf30031b1aa43f3ac7098e6b
Imad Selka <imadselkawi133@gmail.com>
2024-02-10 21:49:59 +0100

    add a constants folder

---
+++ base/constants/constants.py
@@ -0,0 +1,5 @@
+rooms = [
+    {"id": 1, "name": "Conference Room 1", "location": "Building 1, Floor 1"},
+    {"id": 2, "name": "Conference Room 2", "location": "Building 1, Floor 2"},
+    {"id": 3, "name": "Conference Room 3", "location": "Building 2, Floor 3"},
+]
