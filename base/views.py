fe35523014fa544923889f70266039c631187ae8
Imad Selka <imadselkawi133@gmail.com>
2024-02-11 23:50:11 +0100

    feat: home search bar is dynamic and you can search by room name , user name also by description

--- base/views.py
+++ base/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
+    )
+    topics = Topic.objects.all()
+    if not rooms.exists():
+        return render(request, "base/error.html")
+
+    context = {"rooms": rooms, "topics": topics}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {"room": room}
+    context = {
+        "room": room,
+    }
     return render(request, "base/room.html", context)
 
 
