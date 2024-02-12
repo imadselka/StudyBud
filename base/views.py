ad75e86b4934aa884d3345baeb80d1bc7d5a94d1
Imad Selka <imadselkawi133@gmail.com>
2024-02-12 22:57:20 +0100

    add logout func

--- base/views.py
+++ base/views.py
@@ -30,6 +30,11 @@ def loginPage(request):
     return render(request, "base/login_register.html", context)
 
 
+def logoutPage(request):
+    logout(request)
+    return redirect("home")
+
+
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
     rooms = Room.objects.filter(
