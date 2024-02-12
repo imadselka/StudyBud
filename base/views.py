6225a46236e302f35079dcd7c781e0af7251b22a
Imad Selka <imadselkawi133@gmail.com>
2024-02-12 22:40:28 +0100

    add login fnc

--- base/views.py
+++ base/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
 
 # Create your views here.
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "ERROR: user name was not found.")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "ERROR: username or password is incorrect.")
+
+    context = {}
+    return render(request, "base/login_register.html", context)
+
+
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
     rooms = Room.objects.filter(
