447324883beb4493970e3239323d4fb64dba160f
Imad Selka <imadselkawi133@gmail.com>
2024-02-14 22:05:19 +0100

    add Message Form

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
-from .models import Room
+from .models import Room, Message
 
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = "__all__"
+
+
+from django import forms
+
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ("body",)  # Note the use of parentheses instead of square brackets
